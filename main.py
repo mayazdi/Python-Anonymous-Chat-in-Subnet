a997d199f187ff3a8e48d0bc851904edbd7af8fc
Amin <yazdi.mohammad99@gmail.com>
2020-07-14 00:47:55 +0430

    Multithreaded Sending and Receiving

--- main.py
+++ main.py
@@ -1,11 +1,103 @@
+import socket, time
+from threading import Thread
+
+quit_is_called = False
+
 print("Select the mode from the following Choices:\n1)Receiver\n2)Sender")
 mode = input()
 
-if mode=="1": #RCV
+def listen():
+    IP = socket.gethostbyname(socket.gethostname())
+    port = 8080
+    ls = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    rc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ls.bind((IP, port))
+    # while True:
+    data, addr = ls.recvfrom(1024)
+    print(data.decode("UTF-8"), addr)
+    ls.close()
+    rc.sendto(bytes("Hi", "UTF-8"), addr)
+    print("Sent")
+    t0 = Thread(target=run)
+    t1 = Thread(target=rcv_msg, args=("Thread-1", 2, rc))
+    t2 = Thread(target=snd_msg, args=("Thread-2", addr, rc))
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    t0.start()
+    t1.start()
+    t2.start()
+    t0.join()
+
+
+
+def rcv_msg(name, delay, sk):
+    global quit_is_called
+    while True:
+        d, a = sk.recvfrom(1024)
+        print(d.decode("UTF-8"), a)
+        time.sleep(delay)
+        if d.decode("UTF-8")=="quit":
+            quit_is_called = True
+            break
+        
+
+def snd_msg(name, addr, sk):
+    global quit_is_called
+    while True:
+        inp = input()
+        if inp == '\n':
+            continue
+        sk.sendto(bytes(inp, "UTF-8"), addr)
+        if inp == "quit":
+            quit_is_called = True
+            break
+
+
+def run():
+    while True: 
+        global quit_is_called
+        if quit_is_called:
+            break
+
+
+def broadcast():
+    myIP = socket.gethostbyname(socket.gethostname())
+    broadcastIP = socket.inet_ntoa( socket.inet_aton(myIP)[:3] + b'\xff' )
+    port = 8080
+    msg = "Hello"
+    bs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bs.sendto(bytes(msg, "UTF-8"), (broadcastIP, port))
+    data, addr = bs.recvfrom(1024)
+    print(data, addr)
+    #Ta yek daghighe check konim ke jadid oomad block konim
+
+    t0 = Thread(target=run)
+    t1 = Thread(target=rcv_msg, args=("Thread-1", 2, bs))
+    t2 = Thread(target=snd_msg, args=("Thread-2", addr, bs))
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    t0.start()
+    t1.start()
+    t2.start()
+    t0.join()
+    # t1.daemon = True
+    # t2.daemon = True
+    # while 1:
+    #     if quit_is_called:
+    #         break
+    # while True:
+    #     inp = input()
+    #     bs.sendto(bytes(inp, "UTF-8"), addr)
+    #     if inp == "quit":
+    #         break
+
+
+
+if mode=="1": #LSTN
     print("Receiving Selected!")
-    # goto listening mode and answer udp req
-elif mode=="2": #SND
+    listen()
+elif mode=="2": #BRDC
     print("Sending Selected!")
-    # goto sending mode for clients
+    broadcast()
 else:
-    print("The Input")
+    print("The Input is not valid.")
